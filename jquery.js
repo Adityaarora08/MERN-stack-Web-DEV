fcd5c940377f1e79772e09abe5316d008a14e8a0
Adityaarora08 <79107244+Adityaarora08@users.noreply.github.com>
2022-04-15 03:57:06 +0530

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,18 @@
+//get element by id and then change inner html
+$('#elementid').html(" New text");
+
+//change css property
+$("element").css("property name","value given");
+// e.g.
+$("element").css("color","red");
+// or for multiple properties
+$("element").css({
+    width: "100px",
+    height: "239px",
+    backgroundColor:"green"
+
+})
+//event handling
+$("#elemntid").click(function(){
+
+});
